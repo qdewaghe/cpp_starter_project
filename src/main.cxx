00db0295236b31d035a8bc3ed0aa12b1a238bc7c
Jean-Pierre Herlant <q.dewaghe@googlemail.com>
2019-02-08 17:19:15 +0100

    updated gitignore

--- src/main.cxx
+++ src/main.cxx
@@ -1,17 +1,14 @@
 #include <iostream>
-#include <fmt/format.h> 
-
+#include <fmt/format.h>
 #include <boost/asio.hpp>
+
 
 auto main() -> int
 {
-   
    boost::asio::io_context io;
    boost::asio::deadline_timer t(io, boost::posix_time::microseconds(1));
-
-   t.wait();
-
    
+   t.wait();
    std::cout << fmt::format("{}", "test");
 }
 
