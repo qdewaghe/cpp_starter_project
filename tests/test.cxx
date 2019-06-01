dcf49630a2785504e8be619bfe128d3885837711
Quentin Dewaghe <q.dewaghe@googlemail.com>
2019-06-01 15:26:39 +0200

    updated conanfile

--- tests/test.cxx
+++ tests/test.cxx
@@ -1,12 +1,11 @@
 #include <catch2/catch.hpp>
 
-//example test to make sure catch2 was properly pulled by conan 
 int year_to_century(int year)
 {
    return (year + 99) / 100;
 }
 
-TEST_CASE("example")
+TEST_CASE("year to century function")
 {
 	REQUIRE(year_to_century(1905) == 20);
 	REQUIRE(year_to_century(1700) == 17);
