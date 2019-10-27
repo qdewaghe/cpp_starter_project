67d8e843d719ab0c5e9f028334c015bb97d76e11
Quentin Dewaghe <q.dewaghe@googlemail.com>
2019-10-27 12:35:44 +0100

    clang

--- src/example.cpp
+++ src/example.cpp
@@ -8,11 +8,7 @@ auto my_app::year_to_century(int const year) -> int
   }
 
   int const years_per_century = 100;
-<<<<<<< HEAD
-  int const round_up         = 100 - 1;
-=======
   int const round_up          = 100 - 1;
->>>>>>> 6bc98a429915c16f90358e8f6bf58ebd6f9dc2e7
 
   return (year + round_up) / years_per_century;  
 }
--- src/main.cxx
+++ src/main.cxx
@@ -2,24 +2,31 @@
 
 #include "example.hpp"
 
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+#include "llvm/Support/CommandLine.h"
+
 #ifndef NDEBUG
 constexpr auto build_type = "debug";
 #else
 constexpr auto build_type = "release";
 #endif
 
-struct Base {
-   virtual void reimplementMe(int a) {}
-};
-struct Derived : public Base  {
-   virtual void reimplementMe(int a) {}
-};
+static llvm::cl::OptionCategory MyToolCategory("my-tool options");
 
-auto main() -> int
+auto main(int argc, const char** argv) -> int
 {
   const int current_year{2019};
 
   fmt::print("Test: {}\nBuild type: {}\n",
              my_app::year_to_century(current_year),
              build_type);
+
+  using namespace clang::tooling;
+  using namespace llvm;
+  CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
+  ClangTool           Tool(OptionsParser.getCompilations(),
+                 OptionsParser.getSourcePathList());
+  return Tool.run(newFrontendActionFactory<clang::SyntaxOnlyAction>().get());
 }
